e862064ae6ec12c125155e23b6bf52fbb06b80a5
Diffeomorphisme <nicolas.maxant@queenslab.se>
2023-12-03 20:25:28 +0100

    feat(day2)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from template_day import template_day
-from day1 import day1
+# from template_day import template_day
+# from day1 import day1
+from day2 import day2
 
 
 if __name__ == "__main__":
-    day1.run()
+    day2.run()
