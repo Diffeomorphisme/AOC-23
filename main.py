0ef5cffa62a87c9a32d933af1be673a24cddee62
Diffeomorphisme <nicolas.maxant@queenslab.se>
2023-12-03 08:09:34 +0100

    feat: initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from template_day import template_day
+
+
+if __name__ == "__main__":
+    template_day.run()
---
+++ template_day/template_day.py
@@ -0,0 +1,18 @@
+def read_file():
+    with open("template_day/source.txt", "r") as file:
+        for line in file:
+            print(line.strip())
+
+
+def problem_1():
+    pass
+
+
+def problem_2():
+    pass
+
+
+def run():
+    print("Day N")
+    problem_1()
+    problem_2()
